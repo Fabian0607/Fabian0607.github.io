e643d2b778cace4145f10bec10d939bdd1c3594d
Fabian0607 <fabian.beemelmann@gmail.com>
2020-04-16 13:25:02 +0200

    Meine erste Webseite

--- sketch.js
+++ sketch.js
@@ -1,12 +1,7 @@
 function setup() {
-  createCanvas(600, 400);
-  background(255, 0, 0);
-  fill(255, 255, 0)
-  circle(width / 2, height / 2, 300);
-  fill(0)
-  ellipse(width / 2 - 50, height / 2 - 50, 55, 70)
-  ellipse(width / 2 + 50, height / 2 - 50, 55, 70);
-  strokeWeight(5)
-  noFill();
-  arc(width / 2, height / 2 + 50, 80, 50, 0, PI);
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
 }
